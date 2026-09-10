fb0f696eda7a17a05f2d01b93f9e33ee93f5c4db
valoriarpeh <valoriarp_eh@proton.me>
2026-09-10 18:37:37 +0200

    Create webhook-config.js

---
+++ support/webhook-config.js
@@ -0,0 +1,8 @@
+// ====== CONFIG WEBHOOK DISCORD ======
+export const WEBHOOK_URL = "https://discord.com/api/webhooks/1547646891867644046/kA6AqbcQO_Eml1qGgycg7pPiXaTWBXdoT_Mu9a8iHXHiQCPaB95i09mH2hMHgIaBg3q7";
+
+// ⭐ Rôle à mentionner quand un ticket est créé
+export const ROLE_ID = "1546548058760679497";
+
+// ⭐ Lien vers ton site de support
+export const SITE_URL = "https://valoriarpeh.github.io/ValoriaRP/support/";
